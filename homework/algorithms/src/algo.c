32bcd51fb72e763830c92a195f7efe1ec24549eb
Gursimar Virdi <385491@colum.edu>
2023-12-18 17:35:55 -0600

    Wrong Area to put code and fix
    I put the code in the wrong area by accident and I hopefully fixed the code!

--- homework/algorithms/src/algo.c
+++ homework/algorithms/src/algo.c
@@ -1,21 +1,100 @@
 #include "algo.h"
+#include <stdio.h>
+#include <math.h>
 
-long fibonacci(const int n)
+
+long fibonacci (const int n)
 {
-    return 0;
+    if (n == 1)
+    {
+        return 0;
+    }
+    else if (n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 int reverse(const int n)
 {
-    return 0;
+    int reversed = 0;
+    int temp = n;
+
+    while (temp != 0)
+    {
+        reversed = reversed * 10 + temp % 10;
+        temp /= 10;
+    }
+
+    return reversed;
 }
 
-int prime_factor(const int n)
+int is_prime(const int number)
 {
-    return 0;
+    if (number <= 1)
+    {
+        return 0; // Not prime
+    }
+
+    for (int i = 2; i * i <= number; i++)
+    {
+        if (number % i == 0) {
+            return 0; // Not prime
+        }
+    }
+
+    return 1; // Prime
+}
+
+long long prime_factor(long long n)
+{
+    long long LPrime = -1;
+
+    if (n % 2 == 0)
+    {
+        LPrime = 2;
+        while (n % 2 == 0)
+        {
+            n = n / 2;
+        }
+    }
+
+    for (long long i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            LPrime = i;
+            n = n / i;
+        }
+    }
+
+    if (n > 2) {
+        LPrime = n;
+    }
+    return LPrime;
 }
 
 int prime_sum(const int n)
 {
-    return 0;
+    int i, j;
+    int sum = 0;
+
+    for (i = 1; i <= 1000; i++)
+    {
+        for (j = 2; j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                break;
+            }
+        }
+
+        if (i == j) {
+            sum += i;
+        }
+    }
+    
+    return sum;
 }
